6415c00b5ff926b770f4a1bb5cffd8d9a6babe88
woghd9072 <woghd9072@gmail.com>
2022-03-21 16:47:46 +0900

    불필요한 코드 제거

--- lib/controller/isolate_controller.dart
+++ lib/controller/isolate_controller.dart
@@ -1,15 +1,10 @@
 import 'dart:collection';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class IsolateController extends GetxController {
-  final JsonDecoder decoder = const JsonDecoder();
-
-  var widgets = ListView().obs;
-
   Future<List> loadJson() async {
     var json = await rootBundle.loadString("assets/sample.json");
     HashMap<String, List<dynamic>> jsonData = HashMap.from(jsonDecode(json));
